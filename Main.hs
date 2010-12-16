3e4f8cb680588d3cc1d7e5b1964381489e7934d3
Robert Massaioli <robertmassaioli@gmail.com>
2010-12-17 09:38:49 +1100

    Initial import of the HTee Project.

---
+++ Main.hs
@@ -0,0 +1,11 @@
+module Main (main) where
+
+{-
+ - This is a Version of Tee written in Haskell for the Cross Platform and parallel benefits
+ -}
+
+import System.IO
+import System( getArgs )
+import System.Console.GetOpt
+
+ main = getLine >>= putStrLn
