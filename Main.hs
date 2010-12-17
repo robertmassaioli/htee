55e9177dd23535723bc9c4fa9fdbe6be5a28f65b
Robert Massaioli <robertmassaioli@gmail.com>
2010-12-17 11:17:35 +1100

    Started adding in the command line options support.

--- Main.hs
+++ Main.hs
@@ -5,13 +5,34 @@ module Main (main) where
  -}
 
 import System.IO
---import System( getArgs )
+import System.Environment ( getArgs )
 import System.Console.GetOpt
 
 import Control.Monad
 
-main = runTee
+main :: IO ()
+main = do
+  args <- getArgs
+  case getOpt RequireOrder options args of
+      (flags, [],      [])     -> handleFlags flags
+      (_,     nonOpts, [])     -> error $ "unrecognized arguments: " ++ unwords nonOpts
+      (_,     _,       msgs)   -> error $ concat msgs ++ usageInfo header options
 
+data Flag = Version | Help
+
+header :: String
+header = "Usage: htee [options] files..."
+
+options :: [OptDescr Flag] 
+options = [ 
+  Option ['V','v'] ["version"] (NoArg Version) "print program version number",
+  Option ['H','h'] ["help"] (NoArg Help) "print this help message"
+  ]
+
+handleFlags :: [Flag] -> IO ()
+handleFlags _ = runTee
+
+-- The actual code that runs the tee operation
 runTee :: IO ()
 runTee = do
   is_eof <- isEOF
